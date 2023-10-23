c29ed0a2e324c30133e6c936a5f2741d260d54e1
Salomé Menubarbe <salom.22696@gmail.com>
2023-10-23 23:38:54 +0200

    final

---
+++ js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import { LoadPicture } from './loadPicture.js';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/photos';
+const displayElement = document.getElementById('photo-display');
+const loadMoreButton = document.getElementById('load-more-button');
+
+const loadPicture = new LoadPicture(apiUrl, displayElement, 10, 'ASC');
+
+loadPicture.load();
+
+loadMoreButton.addEventListener('click', () => {
+    loadPicture.loadNextSet();
+    
+    if (loadPicture.currentPage * loadPicture.photosPerPage >= loadPicture.photos.length) {
+        loadMoreButton.disabled = true;
+    }
+});
